dfda4a4b97df9cea0be8ee53f42a26e4e1c84e78
MiguelSantos1910 <miguelsantos51346@gmail.com>
2025-12-11 14:14:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,23 @@ const mongoose = require("mongoose");
 const maquinaRoutes = require("./routes/maquinas.js");
 const ordemRoutes = require("./routes/ordemServico.js");
 const usuarioRoutes = require("./routes/usuario.js");
+
 const app = express();
 const porta = process.env.PORT || 10000;
+
 app.use(
     cors({
-        origin: "https://monitoramento-maquinas-frontend.vercel.app" || "http://localhost:5173",
-        methods: "GET, PUT, DELETE, POST",
+        origin: [
+            "https://monitoramento-maquinas-frontend.vercel.app",
+            "http://localhost:5173"
+        ],
+        methods: ["GET", "PUT", "DELETE", "POST"],
         credentials: true,
     })
 );
+
 app.use(express.json());
+
 mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -21,13 +28,11 @@ mongoose
     })
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((err) => console.error("Erro ao conectar:", err));
+
 app.use("/api/maquinas", maquinaRoutes);
 app.use("/api/ordens", ordemRoutes);
-app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/usuarios", usuarioRoutes); // <-- CORREÇÃO AQUI
+
 app.listen(porta, () => {
     console.log(`Servidor rodando em http://localhost:${porta}`);
 });
-
-
-
-
