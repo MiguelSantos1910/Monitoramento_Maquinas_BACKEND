3989a033f9ea2491ef2e91d7bdea32c7a6b798c7
MiguelSantos1910 <miguelsantos51346@gmail.com>
2025-12-11 12:19:12 -0300

    Última Atualização 11.12.25

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const cors = require('cors')
-const mongoose = require('mongoose');
-const maquinaRoutes = require('./routes/maquinas');
-const ordemRoutes = require('./routes/oredemServico');
-const usuarioRoutes = require('./routes/usuario')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const maquinaRoutes = require("./routes/maquinas");
+const ordemRoutes = require("./routes/oredemServico");
+const usuarioRoutes = require("./routes/usuario");
 const app = express();
-const porta = 3000;
-app.use(cors({
-    origin: '*',
-    methods: 'GET, PUT, DELETE, POST',
-    credentials: true
-}));
+const porta = process.env.PORTA || 3000;
+app.use(
+    cors({
+        origin: "https://monitoramento-maquinas-frontend.vercel.app" || "*",
+        methods: "GET, PUT, DELETE, POST",
+        credentials: true,
+    })
+);
 app.use(express.json());
-mongoose.connect("mongodb+srv://admin:Miguel519670@monitoramentomaquinas.028i9fw.mongodb.net/monitoramentoMaquinas?retryWrites=true&w=majority&appName=monitoramentoMaquinas", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Conectado ao MongoDB")).catch(err => console.error("Erro ao conectar:", err));
-app.use('/api/maquinas', maquinaRoutes);
-app.use('/api/ordens', ordemRoutes);
-app.use('/api/usuarios', usuarioRoutes);
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Conectado ao MongoDB"))
+    .catch((err) => console.error("Erro ao conectar:", err));
+app.use("/api/maquinas", maquinaRoutes);
+app.use("/api/ordens", ordemRoutes);
+app.use("/api/usuarios", usuarioRoutes);
 app.listen(porta, () => {
     console.log(`Servidor rodando em http://localhost:${porta}`);
 });
